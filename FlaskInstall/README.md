30b2e47c839f781410fedf0f9d405dcd1fed6656
Nick Tikhonov <nt34@st-andrews.ac.uk>
2015-03-29 20:30:19 +0100

    Added README

---
+++ FlaskInstall/README.md
@@ -0,0 +1,7 @@
+# FlaskInstall
+This script allows you to deploy Flask on a fresh Ubuntu 14.04 installation. Tested on DigitalOcean.
+
+# Download:
+```
+wget https://raw.githubusercontent.com/NickTikhonov/scripts/master/flaskinstall.sh
+```
--- FlaskInstall/flaskinstall.sh
+++ FlaskInstall/flaskinstall.sh
@@ -3,6 +3,9 @@
 # FlaskInstall by Nick Tikhonov
 # installs Apache 2 with Flask on a fresh Ubuntu server
 
+echo "This script will install Apache 2 with Flask on a fresh Ubuntu server. Please type anything to being: "
+read continueinstallation
+
 # Start by installing Apache 
 sudo apt-get update
 sudo apt-get install apache2
