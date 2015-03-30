debf4d524346c85fd774908288afa9285051d093
NickTikhonov <nickanoff@gmail.com>
2015-03-30 13:39:15 +0100

    Fixed MySQL install bug

--- FlaskInstall/flaskinstall.sh
+++ FlaskInstall/flaskinstall.sh
@@ -39,8 +39,10 @@ sudo mkdir static templates
 cat <<EOF > __init__.py
 from flask import Flask
 from flaskext.mysql import MySQL
- 
+
+mysql = MySQL()
 app = Flask(__name__)
+
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = '$mysql_password'
 app.config['MYSQL_DATABASE_DB'] = '$appname'
